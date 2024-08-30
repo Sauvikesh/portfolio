f23f4b3f2c59a907837bb7d19184617267baea2c
Sauvikesh <sauviklal@gmail.com>
2024-08-30 15:20:18 -0700

    feat: created social links in header

--- app/(pages)/_components/Link/Link.tsx
+++ app/(pages)/_components/Link/Link.tsx
@@ -1,16 +1,27 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export type LinkBlockProps = {
-  text: string;
+  text?: string;
   url: string;
+  img: string;
 };
 
 export default function LinkBlock(props: LinkBlockProps) {
-  const { text, url } = props;
+  const { text, url, img } = props;
 
   return (
     <Link href={url}>
-      <div>{text}</div>
+      <div className="flex min-h-11 items-center justify-center bg-[#404040] text-white pt-[10px] pb-[10px] pl-[16px] pr-[16px] rounded-[100px] gap-2">
+        <Image
+          src={img}
+          alt="dummy text :)"
+          width={1000}
+          height={100}
+          className="w-5 h-5"
+        />
+        {text && <p>{text}</p>}
+      </div>
     </Link>
   );
 }
--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,9 +1,45 @@
-import Image from 'next/image';
+// import Image from 'next/image';
+import LinkBlock, { LinkBlockProps } from './_components/Link/Link';
 
 export default function Home() {
+  const Links: LinkBlockProps[] = [
+    {
+      text: 'Resume',
+      url: 'https://www.linkedin.com/feed/',
+      img: '/socials/resume.svg',
+    },
+    {
+      text: 'GitHub',
+      url: 'https://www.linkedin.com/feed/',
+      img: '/socials/github.png',
+    },
+    {
+      text: 'Email',
+      url: 'https://www.linkedin.com/feed/',
+      img: '/socials/email.svg',
+    },
+    {
+      url: 'https://www.linkedin.com/feed/',
+      img: '/socials/linkedin.png',
+    },
+  ];
+
   return (
-    <main className="flex flex-col items-center justify-center bg-black text-white">
-      <h1>Sauvikesh Lal</h1>
+    <main className="flex flex-col items-center justify-center bg-black text-white pt-60">
+      <header className="flex flex-col justify-center items-center">
+        <h1 className="text-[128px] font-semibold">Sauvikesh Lal</h1>
+
+        <div className="flex gap-4">
+          {Links.map((link, index) => (
+            <LinkBlock
+              text={link.text}
+              url={link.url}
+              key={index}
+              img={link.img}
+            />
+          ))}
+        </div>
+      </header>
     </main>
   );
 }
