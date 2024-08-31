10462b468b554377adbd36792e386415ff2f553a
Sauvikesh <sauviklal@gmail.com>
2024-08-30 19:26:22 -0700

    feat: added rest of logo icons

--- app/(pages)/_components/Technologies/TechLogo.tsx
+++ app/(pages)/_components/Technologies/TechLogo.tsx
@@ -1,13 +1,19 @@
-import { RiNextjsFill } from 'react-icons/ri';
+import { ReactNode } from 'react';
 
-export default function TechLogo() {
+export type TechLogoProps = {
+  icon: ReactNode;
+  text: string;
+};
+
+export default function TechLogo(props: TechLogoProps) {
+  const { icon, text } = props;
   return (
-    <div className="relative hover:scale-125 transition-transform duration-300">
+    <div className="relative hover:scale-125 transition-transform duration-300 z-10">
       <div className="icon-wrapper group relative">
         <p className="w-full opacity-0 group-hover:opacity-100 group-hover:-translate-y-12 transition-all duration-300 absolute bg-[#404040] text-white pt-[10px] pb-[10px] pl-[16px] pr-[16px] rounded-[100px] text-center">
-          NextJS
+          {text}
         </p>
-        <RiNextjsFill size={102} />
+        {icon}
       </div>
     </div>
   );
--- app/(pages)/_components/Technologies/Technologies.tsx
+++ app/(pages)/_components/Technologies/Technologies.tsx
@@ -1,21 +1,48 @@
-import TechLogo from './TechLogo';
+import { RiNextjsFill, RiTailwindCssFill } from 'react-icons/ri';
+import {
+  FaHtml5,
+  FaSwift,
+  FaReact,
+  FaCss3Alt,
+  FaNode,
+  FaDocker,
+} from 'react-icons/fa';
+import { SiTypescript, SiSass, SiMongodb } from 'react-icons/si';
+import TechLogo, { TechLogoProps } from './TechLogo';
 
+// TODO: CAN I CHANE THE COLORS OF THE ICONS TO BE WHITE?@?!?!?!?!? PLEASE
 export default function Technologies() {
+  const techLogos: TechLogoProps[] = [
+    { icon: <FaHtml5 size={102} />, text: 'HTML' },
+    {
+      icon: <RiTailwindCssFill size={102} />,
+      text: 'Tailwind',
+    },
+    {
+      icon: <SiTypescript size={102} />,
+      text: 'Typescript',
+    },
+    {
+      icon: <FaSwift size={102} />,
+      text: 'Swifty',
+    },
+    { icon: <FaReact size={102} />, text: 'ReactJS' },
+    { icon: <RiNextjsFill size={102} />, text: 'NextJS' },
+    { icon: <FaCss3Alt size={102} />, text: 'CSS' },
+    {
+      icon: <SiSass size={102} />,
+      text: 'Sass',
+    },
+    { icon: <FaNode size={102} />, text: 'NodeJS' },
+    { icon: <SiMongodb size={102} />, text: 'MongoDB' },
+    { icon: <FaDocker size={102} />, text: 'Docker' },
+  ];
+
   return (
-    <div className="relative flex gap-6 flex-wrap justify-center pb-28 space-x-4">
-      <TechLogo />
-      <TechLogo />
-      <TechLogo />
-      <TechLogo />
-      <TechLogo />
-      <TechLogo />
-
-      <TechLogo />
-      <TechLogo />
-      <TechLogo />
-      <TechLogo />
-      <TechLogo />
-
+    <div className="relative flex gap-6 flex-wrap justify-center pb-28 space-x-4 bg-black">
+      {techLogos.map((logo, index) => (
+        <TechLogo {...logo} key={index} />
+      ))}
     </div>
   );
 }
