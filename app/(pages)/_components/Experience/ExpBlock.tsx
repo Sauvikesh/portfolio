22c7392d8ab202cff892a5d0c7ec29f2483b66ce
Sauvikesh <sauviklal@gmail.com>
2024-08-30 19:39:58 -0700

    feat: added styling to exp grid blocks

--- app/(pages)/_components/Experience/ExpBlock.tsx
+++ app/(pages)/_components/Experience/ExpBlock.tsx
@@ -9,7 +9,7 @@ export default function ExpBlock(props: ExpBlockProps) {
   return (
     <div className="grid grid-cols-4 grid-rows-2 pb-20 gap-y-0">
       <div className="col-span-1">
-        <h2>{time}</h2>
+        <h2 className="text-gray-500">{time}</h2>
       </div>
       <div className="col-span-3">
         <h2>{title}</h2>
@@ -17,7 +17,7 @@ export default function ExpBlock(props: ExpBlockProps) {
 
       <div className="col-span-1"></div>
       <div className="col-span-3">
-        <p>{desc}</p>
+        <p className="text-gray-400">{desc}</p>
       </div>
     </div>
   );
