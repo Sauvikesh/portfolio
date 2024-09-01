856ab8c19f9f3c7397b9e9a472e20baf8e4bbdd6
Sauvikesh <sauviklal@gmail.com>
2024-09-01 00:27:57 -0700

    fix: not as much space between exp title and description

--- app/(pages)/_components/Experience/ExpBlock.tsx
+++ app/(pages)/_components/Experience/ExpBlock.tsx
@@ -8,11 +8,11 @@ export default function ExpBlock(props: ExpBlockProps) {
 
   return (
     <div className="grid grid-cols-4 grid-rows-2 pb-20 xs:pb-10 gap-y-0 xs:flex xs:flex-col">
-      <div className="col-span-1 xs:order-2 xs:pb-4 row-span-1">
-        <h2 className="text-gray-500">{time}</h2>
+      <div className="col-span-1 xs:order-2 xs:pb-4 row-span-1 flex pb-2">
+        <h2 className="text-gray-500 self-end">{time}</h2>
       </div>
-      <div className="col-span-3 xs:order-1 row-span-1">
-        <h2>{title}</h2>
+      <div className="col-span-3 xs:order-1 row-span-1 flex pb-2">
+        <h2 className="self-end">{title}</h2>
       </div>
 
       <div className="col-span-1 row-span-1"></div>
